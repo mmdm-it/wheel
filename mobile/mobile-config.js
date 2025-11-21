62b2f45b61ee06d6674b90e56a2b3f06b77cd036
Howell <gibbens@mmdm.it>
2025-11-21 12:41:38 -0600

    feat: Update changelog for version 0.6.9, enhance parent button breadcrumb logic, and improve volume selector metadata display

--- mobile/mobile-config.js
+++ mobile/mobile-config.js
@@ -4,7 +4,7 @@
  */
 
 const VERSION = {
-    semantic: '0.6.8',      // major.minor.patch
+    semantic: '0.6.9',      // major.minor.patch
     display() {
         return `v${this.semantic}`;
     }
