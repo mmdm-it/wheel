db19fc5a6e98e43cebdb7154629341ebd159f1a9
Howell <gibbens@mmdm.it>
2025-12-15 06:26:45 -0600

    Bump version to 0.8.149 after Phase 1 (ItemUtils extraction)

--- mobile/mobile-config.js
+++ mobile/mobile-config.js
@@ -4,7 +4,7 @@
  */
 
 const VERSION = {
-    semantic: '0.8.148',      // major.minor.patch
+    semantic: '0.8.149',      // major.minor.patch
     display() {
         return `v${this.semantic}`;
     }
